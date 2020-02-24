eb7b081ffc0468eea382cf5b17c36b49db10c101
Jônatas Vitalino <jcv@cin.ufpe.br>
2020-02-23 21:39:20 -0300

    Segunda Migration

---
+++ SalesWebMvc/Models/Enum/SaleStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models.Enum
+{
+    public enum SaleStatus: int
+    {
+        Pending =0,
+        Billed = 1,
+        Cancelled = 2,
+
+    }
+}
