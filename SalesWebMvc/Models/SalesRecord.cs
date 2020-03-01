b56b41b3f91f1410c160ea8861fa0d173b4a7907
Jônatas Vitalino <jcv@cin.ufpe.br>
2020-03-01 01:00:40 -0300

    Implements Grouping Search

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMvc.Models.Enum;
-
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesWebMvc.Models
 {
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString="{0:F2}")]
+        [DataType(DataType.Currency)]
         public double Amount { get; set; }
         public SaleStatus Status {get; set;}
         public Seller Seller { get; set; }
